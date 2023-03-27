3cf138a03f70ca58bc5ffd1124bac058a3704389
Eliot Gronstal <eliot.lauren@gmail.com>
2023-03-27 11:33:29 -0700

    update DestinationsController with Put method and Delete method.
    Co-authored-by: Jacob Maier <jacobamaier@gmail.com>
    Co-Authored-by: Molly Donegan <mollyrdonegan@gmail.com>

--- TravelApi/Controllers/DestinationsController.cs
+++ TravelApi/Controllers/DestinationsController.cs
@@ -43,5 +43,55 @@ namespace TravelApi.Controllers
         await _db.SaveChangesAsync();
         return CreatedAtAction(nameof(GetDestination), new { id = destination.DestinationId }, destination);
     }
+    
+   //PUT: api/Destinations/5
+  	[HttpPut("{id}")]
+ 	 public async Task<IActionResult> Put(int id, Destination destination)
+  	{
+        if (id != destination.DestinationId)
+        {
+            return BadRequest();
+        }
+
+        _db.Destinations.Update(destination);
+
+        try
+        {
+            await _db.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!DestinationExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+        return NoContent();
+  }
+
+   private bool DestinationExists(int id)
+   {
+    return _db.Destinations.Any(location => location.DestinationId == id);
+   }
+
+   //DELETE: api/Destinations/5
+   [HttpDelete("{id}")]
+   public async Task<IActionResult> DeleteDestination(int id)
+   {
+    Destination destination = await _db.Destinations.FindAsync(id);
+    if (destination == null)
+    {
+        return NotFound();
+    }
+
+    _db.Destinations.Remove(destination);
+    await _db.SaveChangesAsync();
+
+    return NoContent();
+   }
   }
 }
